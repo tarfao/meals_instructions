4f5453479bf57767c76274bcb02d9be1b8dea4b8
tarfao <tarfao@hotmail.com>
2021-03-10 19:16:35 -0300

    corrigindo a verificaçao de existencia de meals com o nome enviado

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,9 @@ app.get('/search', (req, res) => {
     //para definir os parâmetros do slice devemos calcular onde ele deve parar.
     limit = parseInt(limit);
     offset = parseInt(offset);
+    strMealSearch = strMealSearch.toUpperCase(); //como o includes é senstive case, faço a verificação com o mesmo tipo de mensagem
     //busco pelas refeições com nomes semelhantes, e retorno apenas o limite informado, para fazer a paginação
-    const mealsFinded = meals_obj.meals.filter(meal => meal.strMeal.includes(strMealSearch)).slice(offset, offset+limit);
+    const mealsFinded = meals_obj.meals.filter(meal => meal.strMeal.toUpperCase().includes(strMealSearch)).slice(offset, offset+limit);
     return res.status(200).json(mealsFinded);
 })
 
