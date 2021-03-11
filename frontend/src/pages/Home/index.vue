84dc06565f72635659fef6976481e275df774e74
tarfao <tarfao@hotmail.com>
2021-03-11 12:32:23 -0300

    adicionando listen no scroll, e utilizando as novas consts

--- frontend/src/pages/Home/index.vue
+++ frontend/src/pages/Home/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="slds-col slds-size_10-of-12 slds-align-center">
+  <div
+    class="slds-col slds-size_10-of-12 slds-align-center"
+    id="home-container"
+  >
     <div class="slds-form-element slds-m-bottom_xx-large slds-m-top_xx-large">
       <div
         class="slds-form-element__control slds-input-has-icon slds-input-has-icon_left-right slds-input-has-icon_group-right"
@@ -27,30 +30,60 @@
 
 <script>
 import axios from "axios";
-import TableMeals from '../../components/TableMeals.vue';
+import TableMeals from "../../components/TableMeals.vue";
+import { LIMIT, URI } from './utils/const'
 
 export default {
   name: "Home",
+
   data() {
     return {
       arr_meals: [],
       searchMeal: "",
     };
   },
+
   components: {
-    "table-meals": TableMeals
+    "table-meals": TableMeals,
   },
+
   async mounted() {
-    const { data } = await axios.get("http://localhost:8000?limit=15&offset=0");
+    const { data } = await axios.get(`${URI}?limit=${LIMIT}&offset=0`);
     this.arr_meals = data;
+
+    window.addEventListener("scroll", this.handleScroll);
   },
+
   watch: {
     searchMeal: async function (strSearch) {
       const { data } = await axios.get(
-        `http://localhost:8000/search?limit=15&offset=0&strMealSearch=${strSearch}`
+        `${URI}/search?limit=${LIMIT}&offset=0&strMealSearch=${strSearch}`
       );
       this.arr_meals = data;
     },
   },
+
+  methods: {
+    handleScroll() {
+      window.addEventListener("scroll", () => {
+        const scrollHeight = document.documentElement.scrollHeight;
+        const scrollTop = document.documentElement.scrollTop;
+        const clientHeight = document.documentElement.clientHeight;
+        //scrollHeight - scrollTop = clientHeight
+        const rows_meals = document.getElementsByClassName('slds-hint-parent');
+        const last_row_meal = rows_meals[rows_meals.length - 1];
+        const scrolling = scrollHeight - scrollTop;
+        const endScrollingPlusHeightRow = clientHeight + last_row_meal.offsetHeight;
+
+        if(scrolling <= endScrollingPlusHeightRow){
+          console.log('scrolllllll')
+        }
+      });
+    },
+  },
+
+  unmounted(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 };
 </script>
